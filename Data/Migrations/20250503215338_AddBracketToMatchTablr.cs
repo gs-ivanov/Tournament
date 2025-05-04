bd40e219ddc8e041c668cf7833c5daed6622ae2d
George <gs.ivanov@abv.bg>
2025-05-04 15:01:28 +0300

    04052025

---
+++ Data/Migrations/20250503215338_AddBracketToMatchTablr.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tournament.Data.Migrations
+{
+    public partial class AddBracketToMatchTablr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bracket",
+                table: "Matches",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Round",
+                table: "Matches",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bracket",
+                table: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "Round",
+                table: "Matches");
+        }
+    }
+}
